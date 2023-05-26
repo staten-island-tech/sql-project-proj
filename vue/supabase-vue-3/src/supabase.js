2dbe58f300077db6398253a7f449520bfe50db08
LydiaZ07 <zhulydia07@gmail.com>
2023-05-26 12:55:29 -0400

    login

--- vue/supabase-vue-3/src/supabase.js
+++ vue/supabase-vue-3/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://crqnwkxbrsiaecqbqmjt.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1cWNzc3J1eWpvbWxscXJla3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4NjA1MDUsImV4cCI6MjAwMDQzNjUwNX0.mPWLUaKp-Ylmqxpm7VArxMMrvOEzB1lpQW8-YERbD-U'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
