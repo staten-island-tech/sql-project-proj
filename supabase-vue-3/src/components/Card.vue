0a4b9a9734c592e6ef5c8b36698d8e788a1b2b98
LydiaZ07 <zhulydia07@gmail.com>
2023-06-07 12:39:03 -0400

    l

---
+++ supabase-vue-3/src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cards">
+        <img class="card-img" v-bind:src='image' alt="">
+        <h1 class="card-title">{{title}}</h1>
+        <p class="card-description">{{description}}</p>
+        <div class="card-line">
+            <h3 class="card-cost">${{cost}}.00</h3>
+        </div>
+        </div>
+</template>
+
+<script>
+  
+export default {
+        name: "Card",
+        props:{
+            title:String,
+       image:String,
+       cost: String,
+       description: String,
+       
+
+        },
+      };
+    ;
+</script>
+
+<style >
+
+</style>
