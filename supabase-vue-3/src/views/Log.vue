6598aaa117237045fe64d2e0b9049edd3dca3b8d
LydiaZ07 <zhulydia07@gmail.com>
2023-06-07 13:13:33 -0400

    l

--- supabase-vue-3/src/views/Log.vue
+++ supabase-vue-3/src/views/Log.vue
@@ -24,12 +24,40 @@ async function signOut() {
     loading.value = false
   }
 }
+
+function makeCard(){
+
+}
 </script>
 
 <template>
  
     <div>
         <h1>Log yo BOOKS >:(</h1>
+        <h1>Build a Cookie(:!</h1>
+    <section class="form-parent">
+        <form id="form">
+            <div class="form-input">
+                <div class="form-input">
+                    <label for="texture">Texture of Cookie</label>
+                    <input type="text" id="input1" class="text-box" required>
+                </div>
+                <div class="form-input">
+                    <label for="extra">Topping</label>
+                    <input type="text" id="input2" class="text-box" required>
+                </div>
+                <div class="type">
+                    <label for="real/fake">Base Flavor</label>
+                    <input type="text" id="input3" class="text-box" required>
+                </div>
+            </div>
+            <div class="hero-form-input">
+                <input type="submit" value="Submit" id="btn">
+            </div>
+        </form>
+    </section>
+
+
       <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
     </div>
 
